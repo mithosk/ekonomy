19a935d7aa9f212f4482312547555ced6ed8bfca
Luca Nicolini <mithosk85@gmail.com>
2026-01-13 19:07:33 +0100

    feat(users): improve edit user error handling

--- app/services/user_services/update_user.rb
+++ app/services/user_services/update_user.rb
@@ -12,27 +12,35 @@ module UserServices
       expense:,
       category:,
       session_user_id:)
-      session_user = User.find_by(id: session_user_id)
-      if session_user.role != "ADMIN" and session_user_id != user_id
-        return { error: "Only admins can edit other users" }
-      end
 
       to_edit_user = User.find_by(id: user_id)
+      session_user = User.find_by(id: session_user_id)
+
+      if session_user.role != "ADMIN"
+        if session_user_id != user_id
+          return { error: "Only admins can edit other users" }
+        end
+
+        if to_edit_user.dashboard != dashboard or
+           to_edit_user.detection != detection or
+           to_edit_user.balancing != balancing or
+           to_edit_user.expense != expense or
+           to_edit_user.category != category
+          return { error: "Only admins can edit permissions" }
+        end
+      end
 
       to_edit_user.username = username
       to_edit_user.full_name = full_name
+      to_edit_user.dashboard = dashboard
+      to_edit_user.detection = detection
+      to_edit_user.balancing = balancing
+      to_edit_user.expense = expense
+      to_edit_user.category = category
 
       if password and password.length > 0
         to_edit_user.password = password
         to_edit_user.password_confirmation = password_confirmation
-      end
-
-      if session_user.role == "ADMIN"
-        to_edit_user.dashboard = dashboard
-        to_edit_user.detection = detection
-        to_edit_user.balancing = balancing
-        to_edit_user.expense = expense
-        to_edit_user.category = category
       end
 
       if to_edit_user.save
--- test/services/user_services/update_user_test.rb
+++ test/services/user_services/update_user_test.rb
@@ -40,11 +40,150 @@ class UserServices::UpdateUserTest < Minitest::Test
     @user_instance_mock.verify
   end
 
-  def test_unit_should_edit_user_without_password_and_without_permissions
-    @user_instance_mock.expect(:username=, nil, [ @faked_username ])
-    @user_instance_mock.expect(:full_name=, nil, [ @faked_full_name ])
+  def test_unit_should_return_error_when_non_admin_edits_dashboard_flag
     @user_instance_mock.expect(:role, "OPERATOR")
+    @user_instance_mock.expect(:dashboard, @faked_dashboard)
+
+    User.stub(:find_by, @user_instance_mock) do
+      result = UserServices::UpdateUser.call(
+        user_id: @faked_user_id,
+        username: @faked_username,
+        password: @faked_password,
+        password_confirmation: @faked_password_confirmation,
+        full_name: @faked_full_name,
+        dashboard: !@faked_dashboard,
+        detection: @faked_detection,
+        balancing: @faked_balancing,
+        expense: @faked_expense,
+        category: @faked_category,
+        session_user_id: @faked_user_id
+      )
+
+      refute_nil result[:error]
+    end
+
+    @user_instance_mock.verify
+  end
+
+  def test_unit_should_return_error_when_non_admin_edits_detection_flag
     @user_instance_mock.expect(:role, "OPERATOR")
+    @user_instance_mock.expect(:dashboard, @faked_dashboard)
+    @user_instance_mock.expect(:detection, @faked_detection)
+
+    User.stub(:find_by, @user_instance_mock) do
+      result = UserServices::UpdateUser.call(
+        user_id: @faked_user_id,
+        username: @faked_username,
+        password: @faked_password,
+        password_confirmation: @faked_password_confirmation,
+        full_name: @faked_full_name,
+        dashboard: @faked_dashboard,
+        detection: !@faked_detection,
+        balancing: @faked_balancing,
+        expense: @faked_expense,
+        category: @faked_category,
+        session_user_id: @faked_user_id
+      )
+
+      refute_nil result[:error]
+    end
+
+    @user_instance_mock.verify
+  end
+
+  def test_unit_should_return_error_when_non_admin_edits_balancing_flag
+    @user_instance_mock.expect(:role, "OPERATOR")
+    @user_instance_mock.expect(:dashboard, @faked_dashboard)
+    @user_instance_mock.expect(:detection, @faked_detection)
+    @user_instance_mock.expect(:balancing, @faked_balancing)
+
+    User.stub(:find_by, @user_instance_mock) do
+      result = UserServices::UpdateUser.call(
+        user_id: @faked_user_id,
+        username: @faked_username,
+        password: @faked_password,
+        password_confirmation: @faked_password_confirmation,
+        full_name: @faked_full_name,
+        dashboard: @faked_dashboard,
+        detection: @faked_detection,
+        balancing: !@faked_balancing,
+        expense: @faked_expense,
+        category: @faked_category,
+        session_user_id: @faked_user_id
+      )
+
+      refute_nil result[:error]
+    end
+
+    @user_instance_mock.verify
+  end
+
+  def test_unit_should_return_error_when_non_admin_edits_expense_flag
+    @user_instance_mock.expect(:role, "OPERATOR")
+    @user_instance_mock.expect(:dashboard, @faked_dashboard)
+    @user_instance_mock.expect(:detection, @faked_detection)
+    @user_instance_mock.expect(:balancing, @faked_balancing)
+    @user_instance_mock.expect(:expense, @faked_expense)
+
+    User.stub(:find_by, @user_instance_mock) do
+      result = UserServices::UpdateUser.call(
+        user_id: @faked_user_id,
+        username: @faked_username,
+        password: @faked_password,
+        password_confirmation: @faked_password_confirmation,
+        full_name: @faked_full_name,
+        dashboard: @faked_dashboard,
+        detection: @faked_detection,
+        balancing: @faked_balancing,
+        expense: !@faked_expense,
+        category: @faked_category,
+        session_user_id: @faked_user_id
+      )
+
+      refute_nil result[:error]
+    end
+
+    @user_instance_mock.verify
+  end
+
+  def test_unit_should_return_error_when_non_admin_edits_category_flag
+    @user_instance_mock.expect(:role, "OPERATOR")
+    @user_instance_mock.expect(:dashboard, @faked_dashboard)
+    @user_instance_mock.expect(:detection, @faked_detection)
+    @user_instance_mock.expect(:balancing, @faked_balancing)
+    @user_instance_mock.expect(:expense, @faked_expense)
+    @user_instance_mock.expect(:category, @faked_category)
+
+    User.stub(:find_by, @user_instance_mock) do
+      result = UserServices::UpdateUser.call(
+        user_id: @faked_user_id,
+        username: @faked_username,
+        password: @faked_password,
+        password_confirmation: @faked_password_confirmation,
+        full_name: @faked_full_name,
+        dashboard: @faked_dashboard,
+        detection: @faked_detection,
+        balancing: @faked_balancing,
+        expense: @faked_expense,
+        category: !@faked_category,
+        session_user_id: @faked_user_id
+      )
+
+      refute_nil result[:error]
+    end
+
+    @user_instance_mock.verify
+  end
+
+  def test_unit_should_edit_user_without_password
+    @user_instance_mock.expect(:username=, nil, [@faked_username])
+    @user_instance_mock.expect(:full_name=, nil, [@faked_full_name])
+    @user_instance_mock.expect(:role, "ADMIN")
+    @user_instance_mock.expect(:dashboard=, nil, [@faked_dashboard])
+    @user_instance_mock.expect(:detection=, nil, [@faked_detection])
+    @user_instance_mock.expect(:balancing=, nil, [@faked_balancing])
+    @user_instance_mock.expect(:expense=, nil, [@faked_expense])
+    @user_instance_mock.expect(:category=, nil, [@faked_category])
     @user_instance_mock.expect(:save, true)
 
     User.stub(:find_by, @user_instance_mock) do
@@ -66,13 +205,17 @@ class UserServices::UpdateUserTest < Minitest::Test
     @user_instance_mock.verify
   end
 
-  def test_unit_should_edit_user_with_password_and_without_permissions
-    @user_instance_mock.expect(:username=, nil, [ @faked_username ])
-    @user_instance_mock.expect(:password=, nil, [ @faked_password ])
-    @user_instance_mock.expect(:password_confirmation=, nil, [ @faked_password_confirmation ])
-    @user_instance_mock.expect(:full_name=, nil, [ @faked_full_name ])
-    @user_instance_mock.expect(:role, "OPERATOR")
-    @user_instance_mock.expect(:role, "OPERATOR")
+  def test_unit_should_edit_user_with_password
+    @user_instance_mock.expect(:username=, nil, [@faked_username])
+    @user_instance_mock.expect(:password=, nil, [@faked_password])
+    @user_instance_mock.expect(:password_confirmation=, nil, [@faked_password_confirmation])
+    @user_instance_mock.expect(:full_name=, nil, [@faked_full_name])
+    @user_instance_mock.expect(:role, "ADMIN")
+    @user_instance_mock.expect(:dashboard=, nil, [@faked_dashboard])
+    @user_instance_mock.expect(:detection=, nil, [@faked_detection])
+    @user_instance_mock.expect(:balancing=, nil, [@faked_balancing])
+    @user_instance_mock.expect(:expense=, nil, [@faked_expense])
+    @user_instance_mock.expect(:category=, nil, [@faked_category])
     @user_instance_mock.expect(:save, true)
 
     User.stub(:find_by, @user_instance_mock) do
@@ -94,75 +237,15 @@ class UserServices::UpdateUserTest < Minitest::Test
     @user_instance_mock.verify
   end
 
-  def test_unit_should_edit_user_without_password_and_with_permissions
-    @user_instance_mock.expect(:username=, nil, [ @faked_username ])
-    @user_instance_mock.expect(:full_name=, nil, [ @faked_full_name ])
+  def test_unit_should_save_user_without_error
+    @user_instance_mock.expect(:username=, nil, [@faked_username])
+    @user_instance_mock.expect(:full_name=, nil, [@faked_full_name])
     @user_instance_mock.expect(:role, "ADMIN")
-    @user_instance_mock.expect(:role, "ADMIN")
-    @user_instance_mock.expect(:dashboard=, nil, [ @faked_dashboard ])
-    @user_instance_mock.expect(:detection=, nil, [ @faked_detection ])
-    @user_instance_mock.expect(:balancing=, nil, [ @faked_balancing ])
-    @user_instance_mock.expect(:expense=, nil, [ @faked_expense ])
-    @user_instance_mock.expect(:category=, nil, [ @faked_category ])
-    @user_instance_mock.expect(:save, true)
-
-    User.stub(:find_by, @user_instance_mock) do
-      UserServices::UpdateUser.call(
-        user_id: @faked_user_id,
-        username: @faked_username,
-        password: nil,
-        password_confirmation: @faked_password_confirmation,
-        full_name: @faked_full_name,
-        dashboard: @faked_dashboard,
-        detection: @faked_detection,
-        balancing: @faked_balancing,
-        expense: @faked_expense,
-        category: @faked_category,
-        session_user_id: @faked_user_id
-      )
-    end
-
-    @user_instance_mock.verify
-  end
-
-  def test_unit_should_edit_user_with_password_and_with_permissions
-    @user_instance_mock.expect(:username=, nil, [ @faked_username ])
-    @user_instance_mock.expect(:password=, nil, [ @faked_password ])
-    @user_instance_mock.expect(:password_confirmation=, nil, [ @faked_password_confirmation ])
-    @user_instance_mock.expect(:full_name=, nil, [ @faked_full_name ])
-    @user_instance_mock.expect(:role, "ADMIN")
-    @user_instance_mock.expect(:role, "ADMIN")
-    @user_instance_mock.expect(:dashboard=, nil, [ @faked_dashboard ])
-    @user_instance_mock.expect(:detection=, nil, [ @faked_detection ])
-    @user_instance_mock.expect(:balancing=, nil, [ @faked_balancing ])
-    @user_instance_mock.expect(:expense=, nil, [ @faked_expense ])
-    @user_instance_mock.expect(:category=, nil, [ @faked_category ])
-    @user_instance_mock.expect(:save, true)
-
-    User.stub(:find_by, @user_instance_mock) do
-      UserServices::UpdateUser.call(
-        user_id: @faked_user_id,
-        username: @faked_username,
-        password: @faked_password,
-        password_confirmation: @faked_password_confirmation,
-        full_name: @faked_full_name,
-        dashboard: @faked_dashboard,
-        detection: @faked_detection,
-        balancing: @faked_balancing,
-        expense: @faked_expense,
-        category: @faked_category,
-        session_user_id: @faked_user_id
-      )
-    end
-
-    @user_instance_mock.verify
-  end
-
-  def test_unit_should_return_no_error_when_user_is_saved
-    @user_instance_mock.expect(:username=, nil, [ @faked_username ])
-    @user_instance_mock.expect(:full_name=, nil, [ @faked_full_name ])
-    @user_instance_mock.expect(:role, "OPERATOR")
-    @user_instance_mock.expect(:role, "OPERATOR")
+    @user_instance_mock.expect(:dashboard=, nil, [@faked_dashboard])
+    @user_instance_mock.expect(:detection=, nil, [@faked_detection])
+    @user_instance_mock.expect(:balancing=, nil, [@faked_balancing])
+    @user_instance_mock.expect(:expense=, nil, [@faked_expense])
+    @user_instance_mock.expect(:category=, nil, [@faked_category])
     @user_instance_mock.expect(:save, true)
 
     User.stub(:find_by, @user_instance_mock) do
@@ -186,14 +269,18 @@ class UserServices::UpdateUserTest < Minitest::Test
     @user_instance_mock.verify
   end
 
-  def test_unit_should_return_error_when_user_is_not_saved
-    @user_instance_mock.expect(:username=, nil, [ @faked_username ])
-    @user_instance_mock.expect(:full_name=, nil, [ @faked_full_name ])
-    @user_instance_mock.expect(:role, "OPERATOR")
-    @user_instance_mock.expect(:role, "OPERATOR")
+  def test_unit_should_save_user_with_error
+    @user_instance_mock.expect(:username=, nil, [@faked_username])
+    @user_instance_mock.expect(:full_name=, nil, [@faked_full_name])
+    @user_instance_mock.expect(:role, "ADMIN")
+    @user_instance_mock.expect(:dashboard=, nil, [@faked_dashboard])
+    @user_instance_mock.expect(:detection=, nil, [@faked_detection])
+    @user_instance_mock.expect(:balancing=, nil, [@faked_balancing])
+    @user_instance_mock.expect(:expense=, nil, [@faked_expense])
+    @user_instance_mock.expect(:category=, nil, [@faked_category])
     @user_instance_mock.expect(:save, false)
     @user_instance_mock.expect(:errors, @user_errors_mock)
-    @user_errors_mock.expect(:full_messages, [ @faked_error ])
+    @user_errors_mock.expect(:full_messages, [@faked_error])
 
     User.stub(:find_by, @user_instance_mock) do
       result = UserServices::UpdateUser.call(
